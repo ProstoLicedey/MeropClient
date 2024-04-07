8a0637906189a9d842c8785997e287bd2b5d9909
Михаил Максимов <mi2004ha@mail.ru>
2024-04-07 23:21:38 +0300

    Initial commit

---
+++ src/components/creator/ModalZal/createZal.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Segmented} from "antd";
+
+const CreateZal = () => {
+    return (
+        <div>
+В разработке
+        </div>
+    );
+};
+
+export default CreateZal;
